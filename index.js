21cdbdb197de33b23fe934b848137c4cf30c0384
Clifton Gockul <clifga@gmail.com>
2023-03-22 15:56:21 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let text = this.getElementById('text')
+    text.textContent = "This is really cool!"
+    console.log("The DOM has loaded");
+  });
